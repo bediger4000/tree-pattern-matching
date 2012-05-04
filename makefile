eb1d5457319651191ea9b96e52386236ee7ab184
bediger <bediger>
2012-05-04 23:44:35 +0000

    *** empty log message ***

---
+++ makefile
@@ -0,0 +1,33 @@
+# $Id$
+CC = cc
+CFLAGS = -I. -g -Wall
+
+# For gcov coverage testing:
+#CFLAGS = -I. -g -Wall -fprofile-arcs -ftest-coverage
+
+OBJS = tree.o buffer.o cb.o aho_corasick.o algorithm_d.o
+
+test6: main.o $(OBJS)
+	$(CC) $(CFLAGS) -o test6 main.o $(OBJS)
+
+main.o: main.c tree.h buffer.h aho_corasick.h algorithm_d.h
+	$(CC) $(CFLAGS) -c -o main.o main.c
+
+tree.o: tree.c tree.h
+	$(CC) $(CFLAGS) -c -o tree.o tree.c
+
+buffer.o: buffer.c buffer.h
+	$(CC) $(CFLAGS) -c -o buffer.o buffer.c
+
+cb.o: cb.c cb.h
+	$(CC) $(CFLAGS) -c -o cb.o cb.c
+
+aho_corasick.o: aho_corasick.c aho_corasick.h cb.h
+	$(CC) $(CFLAGS) -c -o aho_corasick.o aho_corasick.c
+
+algorithm_d.o: algorithm_d.c tree.h algorithm_d.h aho_corasick.h
+	$(CC) $(CFLAGS) -c -o algorithm_d.o algorithm_d.c
+
+clean:
+	-rm -rf *.a *.o *core test?
+	-rm -rf *.gcno *.gcda
---
+++ tree.c
@@ -0,0 +1,164 @@
+/* $Id$ */
+/*
+ * Parse a fully-parenthesized, lisp-like representation of binary trees
+ * into a malloc-allocated C struct tree.
+ * Not the most robust thing in the world, "a b" (rather than "(a b)")
+ * crashes it.  Still, it's compact and fast.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <tree.h>
+
+void print_node(struct tree *n, FILE *output_stream);
+struct tree *new_node(void);
+
+struct tree *
+read_tree(char **str)
+{
+	int looping = 1;
+	struct tree *r = new_node();
+
+	while (looping)
+	{
+		switch (**str)
+		{
+		case ' ':  case '\t': case '\n':
+			switch (r->type)
+			{
+			case UNSET:
+			case INTERIOR:  /* eat whitespace between leaf-node names */
+				break;
+
+			case LEAF:
+				r->label[r->labelsz] = '\0';
+				looping = 0;
+				break;
+			}
+			++*str;
+			break;
+
+		case '(':  /* create an INTERIOR node */
+			switch (r->type)
+			{
+			case UNSET:
+				r->type = INTERIOR;
+				strcpy(r->label, "@");
+				++*str;
+				r->left = read_tree(str);
+				r->right = read_tree(str);
+				break;
+
+			case LEAF:
+				r->label[r->labelsz++] = '\0';
+				looping = 0;
+				/* Doesn't advance *str, so that the calling read_tree()
+				 * creates a new INTERIOR node. */
+				break;
+
+			case INTERIOR:
+				r->right = read_tree(str);
+				looping = 0;
+				break;
+			}
+			break;
+
+		case ')':
+			switch (r->type)
+			{
+			case UNSET:
+				fprintf(stderr, "): syntax error\n");
+				break;
+
+			case LEAF:
+				r->label[r->labelsz++] = '\0';
+				break;
+
+			case INTERIOR:
+				++*str;
+				break;
+			}
+			looping = 0;
+			break;
+
+		default:
+			/* Not '(', ')' or whitespace */
+			switch (r->type)
+			{
+			/* Cases UNSET and LEAF end up reading alll bytes of a multi-
+			 * character leaf-node string into the struct node's label field.
+			 */
+			case UNSET:  /* hits this first time through */
+				r->type = LEAF;
+			case LEAF:   /* hits this every other time through */
+				r->label[r->labelsz++] = *(*str)++;
+				break;
+
+			case INTERIOR:
+				fprintf(stderr, "'%c': syntax error\n", **str);
+				++*str;
+				break;
+			}
+			break;
+		}
+
+	}
+
+	r->tree_size = 1;
+	if (INTERIOR == r->type)
+		r->tree_size += r->left->tree_size + r->right->tree_size;
+
+	return r;
+}
+
+void
+print_tree(struct tree *n, FILE *output_stream)
+{
+	print_node(n, output_stream);
+	printf("\n");
+}
+
+void
+print_node(struct tree *n, FILE *output_stream)
+{
+	switch (n->type)
+	{
+	case LEAF:
+		fprintf(output_stream, "%s", n->label);
+		break;
+	case INTERIOR:
+		printf("(");
+		print_node(n->left, output_stream);
+		printf(" ");
+		print_node(n->right, output_stream);
+		printf(")");
+		break;
+	case UNSET:
+		fprintf(stderr, "Syntax error: printing UNSET node\n");
+		break;
+	}
+}
+
+struct tree *
+new_node(void)
+{
+	struct tree *r = malloc(sizeof(*r));
+
+	r->type = UNSET;
+	r->left = r->right = NULL;
+	r->label[0] = '\0';
+	r->labelsz = 0;
+	r->tree_size = 0;
+
+	return r;
+}
+
+void
+free_tree(struct tree *node)
+{
+	if (node->left) free_tree(node->left);
+	if (node->right) free_tree(node->right);
+	free(node);
+}
+
